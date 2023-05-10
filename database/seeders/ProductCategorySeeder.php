da5784b7de2c930e3ba521218448f25634bb64e1
TytuZx <xtytuspl@gmail.com>
2023-05-10 18:36:31 +0200

    dodano nazwy kategorii

---
+++ database/seeders/ProductCategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ProductCategory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+       $data = [
+            ['name' => 'Pieczywo'],
+            ['name' => 'Przekąski'],
+            ['name' => 'Nabiał'],
+            ['name' => 'Mięso'],
+            ['name' => 'Napoje']
+       ];
+       ProductCategory::insert($data)
+    }
+}
