43adf40791267bae3c0537b8253ad6d6e9edd92f
TytuZx <xtytuspl@gmail.com>
2023-05-13 14:51:06 +0200

    Poprawki końcowe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,16 +7,6 @@
       <div class="container-fluid">
         <div class="row   mb-5">
           <div class="col-12">
-            <div class="dropdown text-md-left text-center float-md-left mb-3 mt-3 mt-md-0 mb-md-0">
-              <label class="mr-2">Sort by:</label>
-              <a class="btn btn-lg btn-light dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Relevance <span class="caret"></span></a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown" x-placement="bottom-start" style="position: absolute; transform: translate3d(71px, 48px, 0px); top: 0px; left: 0px; will-change: transform;">
-                <a class="dropdown-item" href="#">Relevance</a>
-                <a class="dropdown-item" href="#">Price Descending</a>
-                <a class="dropdown-item" href="#">Price Ascending</a>
-                <a class="dropdown-item" href="#">Best Selling</a>
-              </div>
-            </div>
             <div class="btn-group">
               <button type="button" class="btn btn-lg btn-light"> <span class="fa fa-arrow-left"></span> </button>
               <button type="button" class="btn btn-lg btn-light"> <span class="fa fa-arrow-right"></span> </button>
